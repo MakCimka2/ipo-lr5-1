d7e3200669844d91cd2b940e16c5ea225dfe6958
MakCimka <poduskapuska@gmail.com>
2024-11-21 10:21:48 +0300

    main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 n = str(input("Ввведите сроку: "))
 r = len(n)
-glass = "уеёыаоэяию"
-sogl = "йцкнгшщзхфвпрлджчсмтб"
+glass = "уеёыаоэяию" # Все гласные
+sogl = "йцкнгшщзхфвпрлджчсмтб" # Все согласные
 s = 0
 g = 0
-for i in n.lower():
-    if i in glass:
+for i in n.lower(): #Приводим к нижнему регистру
+    if i in glass: # Если есть гласная
         g+=1
-    elif i in sogl:
+    elif i in sogl: # Если есть согласная
         s+=1
-print(s)
-print(g)
-print(r)
+print(s) #Выводим согласные
+print(g) #Выводим гласные  
+print(r) # Выволдим длинну строки
